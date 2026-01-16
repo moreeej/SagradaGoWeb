05cc604f600aa361498b34567be9d011a9ae1a53
Maria Nadine Faye Rufo <nadinerufo7@gmail.com>
2026-01-16 23:01:50 +0800

    Header -- Settings to Profile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -123,7 +123,7 @@ export default function Header() {
                     setShowProfileDropdown(false);
                   }}
                 >
-                  Settings
+                  Profile
                 </button>
                 <button
                   className="dropdown-item"
