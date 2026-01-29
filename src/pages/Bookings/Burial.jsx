e0952c377ecbb4b1a2eee3225295f0d7cae39536
Morej <jeromeaw02@gmail.com>
2026-01-29 22:55:56 +0800

    sge

--- src/pages/Bookings/Burial.jsx
+++ src/pages/Bookings/Burial.jsx
@@ -626,6 +626,7 @@ export default function Burial() {
                     }
                   }}
                 />
+                
 
                 {elem.preview && (
                   <img
